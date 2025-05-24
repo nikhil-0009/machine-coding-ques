7232cb702837cbb6008d09876705da25a20b9cb7
nikhil-0009 <103532543+nikhil-0009@users.noreply.github.com>
2025-05-24 15:40:37 +0530

    renamed folder

---
+++ Otp-Input-component/components/Otp.jsx
@@ -0,0 +1,51 @@
+import React, { useRef, useState } from "react";
+
+const Otp = () => {
+  const [inp, setInp] = useState(["", "", "", ""]);
+  const inputsRef = useRef([]);
+
+
+  const validation = (e,i) => {
+    const value = e.target.value;
+    console.log(value);
+    if (value === '' || /^[0-9]$/.test(value)) {
+      const newInp = [...inp];
+      newInp[i] = value;
+      setInp(newInp);
+      if (value && i < inputsRef.current.length - 1) {
+        inputsRef.current[i + 1].focus();
+      }
+    } 
+
+    
+  };
+  const handleKeyDown = (e, i) => {
+    if (e.key === "Backspace") {
+      if (inp[i] === "" && i > 0) {
+        inputsRef.current[i - 1].focus();
+      }
+    }
+  };
+  return (
+    <>
+      <form action="">
+        {inp.map((e,i)=>{
+          return  <input 
+          key={i}
+          ref={(el) => (inputsRef.current[i] = el)}
+          type="text" 
+          maxLength={1} 
+          onChange={(e) => validation(e, i)} 
+          onKeyDown={(e) => handleKeyDown(e, i)}
+          value={inp[i]} />
+        })}
+        <br /><br />
+        <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ml-40">
+          Submit Otp 
+        </button>  //this change is directly made on Github
+      </form>
+    </>
+  );
+};
+
+export default Otp;
---
+++ Otp-Input-component/src/App.jsx
@@ -0,0 +1,13 @@
+import Otp from '../components/Otp'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Otp/>
+    </>
+  )
+}
+
+export default App
