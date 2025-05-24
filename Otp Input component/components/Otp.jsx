304f2b86eaedff7821ba4d20b1fc003fc815cf5b
nikhil-0009 <103532543+nikhil-0009@users.noreply.github.com>
2025-05-24 14:11:06 +0530

    nothing changed

--- Otp Input component/components/Otp.jsx
+++ Otp Input component/components/Otp.jsx
@@ -41,7 +41,7 @@ const Otp = () => {
         })}
         <br /><br />
         <button className="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded-lg shadow-md transition duration-300 ml-40">
-          Submit
+          Submit 
         </button>
       </form>
     </>
